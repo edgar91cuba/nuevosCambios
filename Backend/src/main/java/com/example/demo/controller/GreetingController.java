424e35dcc59557d7115894693c04842ea04b47a3
edgar91cuba <edgar91cuba@gmail.com>
2024-04-29 11:05:23 +0200

    Errores comunes backend reparados

--- Backend/src/main/java/com/example/demo/controller/GreetingController.java
+++ Backend/src/main/java/com/example/demo/controller/GreetingController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.QueryParam;
 
 @RestController
 public class GreetingController {
@@ -16,11 +15,11 @@ public class GreetingController {
   
 @CrossOrigin(origins="http://localhost:4200")   
 @GetMapping("/saludo")   
-   public String holaMundo(@QueryParam("latitudeOrigen") double latitudeOrigen,
-                           @QueryParam("latitudeDestino") double latitudeDestino,
-                           @QueryParam("longitudOrigen") double longitudOrigen,
-                           @QueryParam("longitudDestino") double longitudDestino){
-       System.println.out(latitudeOrigen,latitudeDestino,longitudOrigen,longitudDestino)
+   public String holaMundo(@RequestParam("latitudeOrigen") double latitudeOrigen,
+                           @RequestParam("latitudeDestino") double latitudeDestino,
+                           @RequestParam("longitudOrigen") double longitudOrigen,
+                           @RequestParam("longitudDestino") double longitudDestino){
+       System.out.println(latitudeOrigen + ", " + latitudeDestino + ", " + longitudOrigen + ", " + longitudDestino);
        String url = "https://api.openchargemap.io/v3/poi/?client=ocm.app.ionic.8.6.1&verbose=false&output=json&includecomments=true&maxresults=40&compact=true&boundingbox=(52.3696,4.8848),(52.3098,5.0396)&key=53f3079e-75c6-40eb-bc30-8b8792c9602f";
         WebClient.Builder builder = WebClient.builder();
         String catFact = builder.build().get().uri(url)
